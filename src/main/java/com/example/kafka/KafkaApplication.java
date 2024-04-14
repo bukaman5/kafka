3b29d844b54a4aba48c34afa2dba758dab52db49
bukaman5 <131355554+bukaman5@users.noreply.github.com>
2024-04-14 16:48:47 +0300

    done

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,7 +1,11 @@
 package com.example.kafka;
 
+import org.apache.kafka.common.protocol.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class KafkaApplication {
@@ -9,5 +13,8 @@ public class KafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
-
+	@Bean
+	public Consumer<String> consumer(){
+		return message -> System.out.println("Message received :: " + message.toUpperCase());
+	}
 }
